517499085419c85553c5301b5a24f7d44609cb58
HazmyAuza <146547556+HazmyAuza@users.noreply.github.com>
2025-07-30 06:47:19 +0700

    Delete BukuKomik directory

--- BukuKomik/footer.php
+++
@@ -1,6 +0,0 @@
-</main>
-    <footer>
-        <p>&copy; <?php echo date("Y"); ?> Hazmy Auza'i. All rights reserved.</p>
-    </footer>
-</body>
-</html>
--- BukuKomik/script.js
+++
@@ -1,5 +0,0 @@
-// Example JavaScript code if needed
-document.addEventListener('DOMContentLoaded', function() {
-    // Add your JavaScript functionality here
-    console.log('Script loaded.');
-});
